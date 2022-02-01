786b7244e7007265dbc4fa55b0ae6239d2ccc9ad
Szev <87762376+Szev@users.noreply.github.com>
2022-02-01 17:42:55 +0530

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,36 @@
+img="";
+status= "";
+
+function setup()
+{
+    canvas= createCanvas(600, 400);
+    canvas.center();
+    objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML= "Status: detecting object";
+}
+function preload()
+{
+    img= loadImage('desk.jpg');
+}
+function draw()
+{
+    image(img, 0 , 0, 600, 400);
+ 
+}
+function modelLoaded()
+{
+    console.log("modelLoaded");
+    status= true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+    }
+}
